409615b95eebfaa1de33fd8fe2091a21ec5485e2
Feichuan <Feichuan05@gamil.com>
2024-09-19 00:19:52 +0800

    11111

--- docs/.vitepress/theme/components/DocFooter.vue
+++ docs/.vitepress/theme/components/DocFooter.vue
@@ -19,7 +19,7 @@ const gitalk = new Gitalk({
     repo:'QSSS',
     owner:'ifeichuan',
     admin:['ifeichuan'],
-    id:'Web comments',
+    id:useRoute().data.title,
     title: useRoute().data.title
 })
 onMounted(()=>{
