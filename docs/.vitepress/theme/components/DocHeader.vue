2f3a0ccb0e16293693715132921b73afe00b9a7d
Feichuan <Feichuan05@gamil.com>
2024-09-18 23:13:19 +0800

    添加padding

--- docs/.vitepress/theme/components/DocHeader.vue
+++ docs/.vitepress/theme/components/DocHeader.vue
@@ -25,9 +25,10 @@ span{
     background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%,rgba(11, 105, 200,0.5) 50%, rgb(11, 105, 200,0.5) 100%);
   /* padding: 5px; */
   position: relative;
-  padding-bottom: 2px;
+  padding: 2px;
   outline: 1px;
   border-radius: 10px;
+
 }
 .header{
     margin-bottom: 50px;
