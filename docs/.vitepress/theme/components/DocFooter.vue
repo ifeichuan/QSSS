287224c7c181e639a56ff2f9313e0830f3b9174b
Feichuan <Feichuan05@gamil.com>
2024-09-19 00:18:55 +0800

    1111

--- docs/.vitepress/theme/components/DocFooter.vue
+++ docs/.vitepress/theme/components/DocFooter.vue
@@ -9,13 +9,18 @@
 import'gitalk/dist/gitalk.css'
 import Gitalk from 'gitalk';
 import { onMounted } from 'vue';
+import { useRoute } from 'vitepress';
+
+// console.log(useRoute().data.title);
+
 const gitalk = new Gitalk({
     clientID:'Ov23liwzcxCw5gaywY5K',
     clientSecret:'4f8318a101f427a94c16c1c0bde57b48a89afab6',
     repo:'QSSS',
     owner:'ifeichuan',
     admin:['ifeichuan'],
-    id:'Web comments'
+    id:'Web comments',
+    title: useRoute().data.title
 })
 onMounted(()=>{
     gitalk.render("gitalk-container")
