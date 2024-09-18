d97d946e9c6d7e70796193a70f0fd13e9177f797
Feichuan <Feichuan05@gamil.com>
2024-09-19 01:18:31 +0800

    copy to zaun

--- docs/.vitepress/theme/components/DocFooter.vue
+++ docs/.vitepress/theme/components/DocFooter.vue
@@ -6,26 +6,38 @@
 </template>
 
 <script lang='ts' setup>
-import'gitalk/dist/gitalk.css'
-import Gitalk from 'gitalk';
-import { onMounted } from 'vue';
-import { useRoute,onContentUpdated } from 'vitepress';
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+import { onContentUpdated, useRouter } from "vitepress";
 
-// console.log(useRoute().data.title);
-
-
-onMounted(()=>{
-    const gitalk = new Gitalk({
+// const { route, go } = useRouter();
+function deleteChild(element: HTMLDivElement | null) {
+  let child = element?.lastElementChild;
+  while (child) {
+    element?.removeChild(child);
+    child = element?.lastElementChild;
+  }
+}
+onContentUpdated(() => {
+  // reset gittalk element for update
+  const element = document.querySelector("#gitalk-container");
+  if (!element) {
+    return;
+  }
+  deleteChild(element);
+  const gitalk = new Gitalk({
     clientID:'Ov23liwzcxCw5gaywY5K',
     clientSecret:'4f8318a101f427a94c16c1c0bde57b48a89afab6',
     repo:'QSSS',
     owner:'ifeichuan',
     admin:['ifeichuan'],
-    id:useRoute().data.title,
-    title:'WebComments' 
-})
-    gitalk.render("gitalk-container")
-})
+    id: location.pathname.substring(0, 50),
+    // title:'WebComments' ,
+    language: "zh-CN",
+    distractionFreeMode: true, // Facebook-like distraction free mode
+  });
+  gitalk.render("gitalk-container");
+});
 </script>
 
 <style>
